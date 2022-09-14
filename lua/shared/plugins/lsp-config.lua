712a60fb050ce7ad8bbd2a4da0f30a6545bd9df1
Elijah Mirecki <elimirks@gmail.com>
2022-09-14 16:31:50 -0700

    Add borders to LSP popup buffers

--- lua/shared/plugins/lsp-config.lua
+++ lua/shared/plugins/lsp-config.lua
@@ -60,3 +60,24 @@ local make_setup_handlers = function ()
 end
 
 require("mason-lspconfig").setup_handlers(make_setup_handlers())
+
+vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+
+local border = {
+    {"┏", "FloatBorder"},
+    {"━", "FloatBorder"},
+    {"┓", "FloatBorder"},
+    {"┃", "FloatBorder"},
+    {"┛", "FloatBorder"},
+    {"━", "FloatBorder"},
+    {"┗", "FloatBorder"},
+    {"┃", "FloatBorder"},
+}
+
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+    opts = opts or {}
+    opts.border = opts.border or border
+    return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
