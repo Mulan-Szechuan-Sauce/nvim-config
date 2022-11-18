26619c8fcae55e288e96e14b3cac569878cd2dcc
Josh Wolfe <joshuafwolfe@gmail.com>
2022-11-18 11:25:09 -0500

    Add ability to narrow to a function using treesitter

--- lua/shared/functions.lua
+++ lua/shared/functions.lua
@@ -52,3 +52,53 @@ function get_sourcegraph_url()
 
     vim.fn.setreg('+', url)
 end
+
+local function open_current_tsnode_in_scratch_buf()
+    local ts_utils = require('nvim-treesitter.ts_utils')
+    local node = ts_utils.get_node_at_cursor()
+
+    while node do
+        local type = node:type()
+        if type == 'function_declaration' or
+            type == 'function_item' or
+            type == 'method_declaration' then
+            break
+        else
+            node = node:parent()
+        end
+    end
+
+    if not node then return end
+
+    local start_row, start_col, end_row, end_col = ts_utils.get_node_range(node)
+    local text = vim.treesitter.query.get_node_text(node, 0, { concat = false })
+
+    local original_buf = vim.api.nvim_get_current_buf()
+    local ft = vim.bo.filetype
+    local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
+
+    local tmp_buf = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_buf_set_lines(tmp_buf, 0, -1, true, text)
+    vim.api.nvim_buf_set_option(tmp_buf, 'filetype', ft)
+    vim.api.nvim_set_current_buf(tmp_buf)
+    vim.api.nvim_win_set_cursor(0, { cursor_row - start_row, cursor_col })
+    vim.api.nvim_buf_set_name(tmp_buf, '*narrow-tmp*')
+
+    vim.api.nvim_create_autocmd({'BufDelete'}, {
+        pattern = {'<buffer>'},
+        callback = function()
+            local buffer_content = vim.api.nvim_buf_get_lines(tmp_buf, 0, -1, false)
+
+            vim.api.nvim_buf_set_text(original_buf, start_row, start_col, end_row, end_col, buffer_content)
+        end,
+    })
+end
+
+function narrow_to_function()
+    if string.find(vim.api.nvim_buf_get_name(0), '*narrow-tmp*', nil, true) then
+        vim.api.nvim_buf_delete(0, {})
+    else
+        open_current_tsnode_in_scratch_buf()
+    end
+end
+
