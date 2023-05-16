a027389e59adeef0fdcf669299c62462dc651a6f
Faiz-C <faiz.chaudhry97@outlook.com>
2023-05-15 22:18:40 -0400

    update bootstrap.lua to properly add two both lists of plugins together for lazy to load

--- lua/shared/bootstrap.lua
+++ lua/shared/bootstrap.lua
@@ -14,6 +14,14 @@ vim.opt.rtp:prepend(lazypath)
 local lazy = require('lazy')
 local plugins = require('shared.plugins')
 local user_plugins = user_install_plugins()
-lazy.setup(vim.tbl_extend('force', plugins, user_plugins))
+
+-- Updated this to be list_extends instead of tbl_extends because lists in lua are tables with key = array index and when
+-- performing an extend using tbl.extend we don't actually get all values from both tables. Given length of user_plugins
+-- is N we get the following: 
+--   1) All plugins from user_plugins
+--   2) plugins with index > N from the shared plugins
+-- This function actually gives us all from both
+vim.list_extend(plugins, user_plugins)
+lazy.setup(plugins)
 
 
