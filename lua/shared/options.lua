fb27b3ae2b8011aa29176b2673e4212d31aa9c6c
Josh Wolfe <wolfe@dropbox.com>
2024-04-11 13:06:37 -0400

    Enable smartcase. Tbh I thought this was on the entire time

--- lua/shared/options.lua
+++ lua/shared/options.lua
@@ -9,6 +9,7 @@ vim.o.smartindent = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.ignorecase = true
+vim.o.smartcase = true
 vim.o.scrolloff = 10
 vim.o.splitbelow = true
 vim.o.splitright = true
