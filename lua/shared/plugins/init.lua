464466395a32ec9134f7fae7cc96d5a405f3431e
Josh Wolfe <joshuafwolfe@gmail.com>
2025-12-03 23:22:34 -0500

    Focus trouble by default

--- lua/shared/plugins/init.lua
+++ lua/shared/plugins/init.lua
@@ -113,7 +113,9 @@ return {
     'folke/trouble.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-        require('trouble').setup();
+        require('trouble').setup({
+            focus = true
+        });
     end
 },
 
