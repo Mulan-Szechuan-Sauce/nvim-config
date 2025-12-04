04b6750a911f578dc8695ae6d80650d62313d72d
Josh Wolfe <joshuafwolfe@gmail.com>
2025-12-04 10:15:16 -0500

    Add dotnet mason registry

--- lua/shared/plugins/lsp-config.lua
+++ lua/shared/plugins/lsp-config.lua
@@ -71,7 +71,12 @@ vim.lsp.config('svelte', {
     end,
 })
 
-require('mason').setup()
+require('mason').setup({
+    registries = {
+        'github:mason-org/mason-registry',
+        'github:Crashdummyy/mason-registry',
+    },
+})
 require('mason-lspconfig').setup()
 
 
